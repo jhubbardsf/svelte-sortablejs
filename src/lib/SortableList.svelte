4329cf3adf3c78a4757b8c296b2b860b93043c36
Josh Hubbard <jhubbardsf@gmail.com>
2022-05-04 11:51:32 -0400

    Update example to use new options

--- src/lib/SortableList.svelte
+++ src/lib/SortableList.svelte
@@ -1,15 +1,160 @@
 <script lang="ts">
 	import { onDestroy } from 'svelte';
-	import Sortable, { MultiDrag, Swap, type Options } from 'sortablejs';
+	import Sortable, { MultiDrag, Swap, type GroupOptions, type Options } from 'sortablejs';
+	import type { SortableEvent } from 'sortablejs';
+	import type { MoveEvent } from 'sortablejs';
 
+	// Svelte Library Variables
 	let list: HTMLElement;
 	let sortable: Sortable;
 	let className: string;
 	export { className as class };
-	export let options: Options | undefined = {};
+	export let options: Options = {};
+
+	// Default Options
+	export let group: string | GroupOptions | undefined = undefined;
+	export let sort = true;
+	export let disabled = false;
+	// export let store:
+	// 	| {
+	// 			get: (sortable: Sortable) => string[];
+	// 			set: (sortable: Sortable) => void;
+	// 	  }
+	// 	| undefined = undefined;
+	export let handle: string | undefined = undefined;
+	export let swapThreshold = 1;
+	// percentage; 0 <= x <= 1
+	export let invertSwap = false;
+	// invert always
+	// export let invertedSwapThreshold: number | undefined = undefined;
+	// will be set to same as swapThreshold if default
+	export let removeCloneOnHide = true;
+	export let ghostClass = 'sortable-ghost';
+	export let chosenClass = 'sortable-chosen';
+	export let dragClass = 'sortable-drag';
+	export let ignore = 'a; img';
+	export let filter:
+		| string
+		| ((
+				this: Sortable,
+				event: Event | TouchEvent,
+				target: HTMLElement,
+				sortable: Sortable
+		  ) => boolean)
+		| undefined = undefined;
+	export let preventOnFilter = true;
+	export let animation = 0;
+	/**
+	 * Easing for animation. Defaults to null.
+	 *
+	 * See https://easings.net/ for examples.
+	 *
+	 * For other possible values, see
+	 * https://www.w3schools.com/cssref/css3_pr_animation-timing-function.asp
+	 *
+	 * @example
+	 *
+	 * // CSS functions
+	 * | 'steps(int, start | end)'
+	 * | 'cubic-bezier(n, n, n, n)'
+	 *
+	 * // CSS values
+	 * | 'linear'
+	 * | 'ease'
+	 * | 'ease-in'
+	 * | 'ease-out'
+	 * | 'ease-in-out'
+	 * | 'step-start'
+	 * | 'step-end'
+	 * | 'initial'
+	 * | 'inherit'
+	 */
+	// export let easing: string | undefined = undefined;
+	export let dropBubble = false;
+	export let dragoverBubble = false;
+	export let dataIdAttr = 'data-id';
+	export let delay = 0;
+	export let delayOnTouchOnly = false;
+	export let forceFallback = false;
+	export let fallbackClass = 'sortable-fallback';
+	export let fallbackOnBody = false;
+	export let fallbackTolerance = 0;
+	export let fallbackOffset = {
+		x: 0,
+		y: 0
+	};
+	export let emptyInsertThreshold = 5;
+	export let direction: 'horizontal' | 'vertical' | undefined = undefined;
+	// export let touchStartThreshold: number | undefined = undefined;
+	// export let setData:
+	// 	| ((dataTransfer: DataTransfer, draggedElement: HTMLElement) => void)
+	// 	| undefined = undefined;
+	// export let draggable: string | undefined = undefined;
+	// export let onChoose: ((event: SortableEvent) => void) | undefined = undefined;
+	// export let onUnchoose: ((event: SortableEvent) => void) | undefined = undefined;
+	// export let onStart: ((event: SortableEvent) => void) | undefined = undefined;
+	// export let onEnd: ((event: SortableEvent) => void) | undefined = undefined;
+	// export let onAdd: ((event: SortableEvent) => void) | undefined = undefined;
+	// export let onUpdate: ((event: SortableEvent) => void) | undefined = undefined;
+	// export let onRemove: ((event: SortableEvent) => void) | undefined = undefined;
+	// export let onFilter: ((event: SortableEvent) => void) | undefined = undefined;
+	// export let onSort: ((event: SortableEvent) => void) | undefined = undefined;
+	// export let onClone: ((event: SortableEvent) => void) | undefined = undefined;
+	// export let onMove:
+	// 	| ((evt: MoveEvent, originalEvent: Event) => boolean | -1 | 1 | void)
+	// 	| undefined = undefined;
+	// export let onChange: ((event: SortableEvent) => void) | undefined = undefined;
 
 	$: if (list) {
-		sortable = Sortable.create(list, options);
+		options = {
+			group,
+			sort,
+			disabled,
+			// store,
+			handle,
+			swapThreshold,
+			invertSwap,
+			// invertedSwapThreshold,
+			removeCloneOnHide,
+			ghostClass,
+			chosenClass,
+			dragClass,
+			ignore,
+			filter,
+			preventOnFilter,
+			animation,
+			// easing,
+			dropBubble,
+			dragoverBubble,
+			dataIdAttr,
+			delay,
+			delayOnTouchOnly,
+			forceFallback,
+			fallbackClass,
+			fallbackOnBody,
+			fallbackTolerance,
+			fallbackOffset,
+			emptyInsertThreshold,
+			direction,
+			// touchStartThreshold,
+			// setData,
+			// draggable,
+			// onChoose,
+			// onUnchoose,
+			// onStart,
+			// onEnd,
+			// onAdd,
+			// onUpdate,
+			// onRemove,
+			// onFilter,
+			// onSort,
+			// onClone,
+			// onMove,
+			// onChange,
+			...options
+		};
+
+		sortable = Sortable.create(list, { ...options });
 	}
 
 	onDestroy(() => {
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,18 +10,18 @@
 	library.add(faArrowsAlt);
 
 	type ItemList = { item: string; filtered?: boolean }[];
-	type SelectOption = { isHorizontal: boolean; text: string };
+	type SelectOption = { isHorizontal: boolean; text: 'horizontal' | 'vertical' };
 
 	let invertSwap = false;
-	let isHorizontal = true;
-	let options: Options[] = Array(8);
 	let selectOptions: SelectOption[] = [
-		{ isHorizontal: true, text: 'Horizontal' },
-		{ isHorizontal: false, text: 'Vertical' }
+		{ isHorizontal: true, text: 'horizontal' },
+		{ isHorizontal: false, text: 'vertical' }
 	];
 	let sliderValue: [number, number] = [0.5, 1];
-	let selected = selectOptions[0];
+	$: selected = selectOptions[0];
 	$: boxWidth = 200 * sliderValue[0];
+
+	$: console.log({ selected });
 
 	const exampleItems = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'];
 	const exampleItemsFilter: ItemList = [
@@ -32,73 +32,6 @@
 		{ item: 'Item 5' },
 		{ item: 'Item 6' }
 	];
-
-	const updateSelection = () => {
-		if (selected) isHorizontal = selected.isHorizontal;
-	};
-
-	// Example 1
-	options[0] = {
-		animation: 150,
-		ghostClass: 'bg-info'
-	};
-
-	// Example 2
-	options[1] = {
-		group: 'shared2', // set both lists to same group
-		animation: 150
-	};
-
-	// Example 3
-	options[2] = {
-		group: {
-			name: 'shared3',
-			pull: 'clone' // To clone: set pull to 'clone'
-		},
-		animation: 150
-	};
-
-	// Example 4
-	options[3] = {
-		group: {
-			name: 'shared4',
-			pull: 'clone',
-			put: false // Do not allow items to be put into this list
-		},
-		animation: 150,
-		sort: false // To disable sorting: set sort to false
-	};
-
-	options[4] = {
-		group: 'shared4',
-		animation: 150
-	};
-
-	// Example 5 - Handle
-	options[5] = {
-		handle: '.cursor-grab', // handle's class
-		animation: 150
-	};
-
-	// Example 6 - Filtered
-	options[6] = {
-		filter: '.filtered', // 'filtered' class is not draggable
-		animation: 150
-	};
-
-	// Example 7 - Threshold
-	$: options[7] = {
-		swapThreshold: sliderValue[0],
-		animation: 150
-	};
-
-	// Example 8 - Nesting
-	options[8] = {
-		group: 'nested',
-		animation: 150,
-		forceFallback: true,
-		swapThreshold: 0.65
-	};
 </script>
 
 <div class="container mb-4">
@@ -110,7 +43,7 @@
 	<div id="simple-list" class="mt-4 border rounded p-6">
 		<h2 class="font-bold">Example 1 - Simple List</h2>
 
-		<SortableList class="list-group col" options={options[0]}>
+		<SortableList class="list-group col" animation={150} ghostClass="bg-info">
 			{#each exampleItems as item}
 				<div class="list-group-item">
 					{item}
@@ -123,13 +56,13 @@
 		<h2 class="font-bold">Example 2 - Shared Lists</h2>
 
 		<div class="flex">
-			<SortableList class="list-group col mr-6" options={options[1]}>
+			<SortableList class="list-group col mr-6" group="shared2" animation={150}>
 				{#each exampleItems as item}
 					<div class="list-group-item bg-secondary text-white">{item} - Group 1</div>
 				{/each}
 			</SortableList>
 
-			<SortableList class="list-group col" options={options[1]}>
+			<SortableList class="list-group col" group="shared2" animation={150}>
 				{#each exampleItems as item}
 					<div class="list-group-item">{item} - Group 2</div>
 				{/each}
@@ -141,13 +74,21 @@
 		<h2 class="font-bold">Example 3 - Cloning Lists</h2>
 
 		<div class="flex">
-			<SortableList class="list-group col mr-6" options={options[2]}>
+			<SortableList
+				class="list-group col mr-6"
+				group={{ name: 'shared3', pull: 'clone' }}
+				animation={150}
+			>
 				{#each exampleItems as item}
 					<div class="list-group-item bg-secondary text-white">{item} - Group 1</div>
 				{/each}
 			</SortableList>
 
-			<SortableList class="list-group col" options={options[2]}>
+			<SortableList
+				class="list-group col"
+				group={{ name: 'shared3', pull: 'clone' }}
+				animation={150}
+			>
 				{#each exampleItems as item}
 					<div class="list-group-item">{item} - Group 2</div>
 				{/each}
@@ -159,13 +100,18 @@
 		<h2 class="font-bold">Example 4 - Disable Sort/Clone on Left</h2>
 
 		<div class="flex">
-			<SortableList class="list-group col mr-6" options={options[3]}>
+			<SortableList
+				class="list-group col mr-6"
+				group={{ name: 'shared4', pull: 'clone', put: false }}
+				animation={150}
+				sort={false}
+			>
 				{#each exampleItems as item}
 					<div class="list-group-item bg-secondary text-white">{item} - Group 1</div>
 				{/each}
 			</SortableList>
 
-			<SortableList class="list-group col" options={options[4]}>
+			<SortableList class="list-group col" group="shared4" animation={150}>
 				{#each exampleItems as item}
 					<div class="list-group-item">{item} - Group 2</div>
 				{/each}
@@ -176,7 +122,7 @@
 	<div id="handle-list" class="mt-4 border rounded p-6">
 		<h2 class="font-bold">Example 5 - Handle</h2>
 
-		<SortableList class="list-group col" options={options[5]}>
+		<SortableList class="list-group col" handle=".cursor-grab" animation={150}>
 			{#each exampleItems as item}
 				<div class="list-group-item">
 					<FontAwesomeIcon icon={faArrowsAlt} class="cursor-grab" />
@@ -189,7 +135,7 @@
 	<div id="filter-list" class="mt-4 border rounded p-6">
 		<h2 class="font-bold">Example 6 - Filter</h2>
 
-		<SortableList class="list-group col" options={options[6]}>
+		<SortableList class="list-group col" filter=".filtered" animation={150}>
 			{#each exampleItemsFilter as item}
 				<div
 					class="list-group-item"
@@ -209,11 +155,11 @@
 			Current Threshold: {sliderValue[0]}
 			<Slider min="0" max="1" step=".01" bind:value={sliderValue} />
 			<div>
-				Invert Swap: <input type="checkbox" bind:checked={invertSwap} />
+				Invert Swap ({invertSwap}): <input type="checkbox" bind:checked={invertSwap} />
 			</div>
 			<div>
 				Is Horizontal?:
-				<select bind:value={selected} on:change={updateSelection}>
+				<select bind:value={selected}>
 					{#each selectOptions as option}
 						<option value={option}>
 							{option.text}
@@ -222,14 +168,42 @@
 				</select>
 			</div>
 		</div>
-		{#key options}
-			<SortableList class="flex" options={options[7]}>
-				<div class="h-[200px] w-[200px] border border-solid  mr-4 mt-4">
-					<div class="bg-red-500 m-auto h-full" style="width: {boxWidth}px;" />
+		{#key [sliderValue, invertSwap, selected]}
+			<SortableList
+				class={selected?.isHorizontal ? 'flex' : ''}
+				swapThreshold={sliderValue[0]}
+				animation={150}
+				{invertSwap}
+				direction={selected?.text}
+			>
+				<div
+					class="{selected?.isHorizontal
+						? ''
+						: 'mx-auto'} h-[200px] w-[200px] border border-solid  mr-4 mt-4"
+				>
+					{#if !invertSwap}
+						<div class="bg-red-500 m-auto h-full" style="width: {boxWidth}px;" />
+					{:else}
+						<div class="h-full flex justify-between">
+							<div class="bg-red-500 h-full" style="width: {boxWidth / 2}px;" />
+							<div class="bg-red-500 h-full" style="width: {boxWidth / 2}px;" />
+						</div>
+					{/if}
 				</div>
 
-				<div class="h-[200px] w-[200px] border border-solid mr-4 mt-4">
-					<div class="bg-blue-500 m-auto h-full" style="width: {boxWidth}px;" />
+				<div
+					class="{selected?.isHorizontal
+						? ''
+						: 'mx-auto'} h-[200px] w-[200px] border border-solid mr-4 mt-4"
+				>
+					{#if !invertSwap}
+						<div class="bg-blue-500 m-auto h-full" style="width: {boxWidth}px;" />
+					{:else}
+						<div class="h-full flex justify-between">
+							<div class="bg-blue-500 h-full" style="width: {boxWidth / 2}px;" />
+							<div class="bg-blue-500 h-full" style="width: {boxWidth / 2}px;" />
+						</div>
+					{/if}
 				</div>
 			</SortableList>
 		{/key}
@@ -238,14 +212,32 @@
 	<div id="nested-lists" class="mt-4 border rounded p-6">
 		<h2 class="font-bold">Example 7 - Nesting</h2>
 
-		<SortableList class="list-group col nested-sortable" options={options[8]}>
+		<SortableList
+			class="list-group col nested-sortable"
+			group="nested"
+			animation={150}
+			forceFallback={true}
+			swapThreshold={0.65}
+		>
 			<div class="list-group-item nested-1">
 				Item 1.1
-				<SortableList class="list-group col nested-sortable" options={options[8]}>
+				<SortableList
+					class="list-group col nested-sortable"
+					group="nested"
+					animation={150}
+					forceFallback={true}
+					swapThreshold={0.65}
+				>
 					<div class="list-group-item nested-2">Item 2.1</div>
 					<div class="list-group-item nested-2">
 						Item 2.2
-						<SortableList class="list-group col nested-sortable" options={options[8]}>
+						<SortableList
+							class="list-group col nested-sortable"
+							group="nested"
+							animation={150}
+							forceFallback={true}
+							swapThreshold={0.65}
+						>
 							<div class="list-group-item nested-3">Item 3.1</div>
 							<div class="list-group-item nested-3">Item 3.2</div>
 							<div class="list-group-item nested-3">Item 3.3</div>
@@ -260,7 +252,13 @@
 			<div class="list-group-item nested-1">Item 1.3</div>
 			<div class="list-group-item nested-1">
 				Item 1.4
-				<SortableList class="list-group col nested-sortable" options={options[8]}>
+				<SortableList
+					class="list-group col nested-sortable"
+					group="nested"
+					animation={150}
+					forceFallback={true}
+					swapThreshold={0.65}
+				>
 					<div class="list-group-item nested-2">Item 2.1</div>
 					<div class="list-group-item nested-2">Item 2.2</div>
 					<div class="list-group-item nested-2">Item 2.3</div>
